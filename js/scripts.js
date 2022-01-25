fc2d885627095834ce95a1931c36e8ed1647bbe5
Yazmin Perez <zoeperez333@gmail.com>
2022-01-25 09:59:16 -0800

    create name, description, date, start/end times sections in html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("form#name").submit(function(event) {
+    event.preventDefault();
+    const name1Input = $("input#name1");
+  });
+});
+
+$(document).ready(function() {
+  $("form#description").submit(function(event) {
+    event.preventDefault();
+    const description1Input = $("input#description1");
+  });
+});

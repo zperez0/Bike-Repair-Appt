3a5b78ee81d7ce3ec77afa7213855d72a500b94e
Yazmin Perez <zoeperez333@gmail.com>
2022-01-25 10:06:29 -0800

    Add jQuery user interface logic for each section

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
   $("form#name").submit(function(event) {
     event.preventDefault();
-    const name1Input = $("input#name1");
+    const name1Input = $("input#name1").val();
+    $(".name1").text(name1Input);
   });
 });
 
 $(document).ready(function() {
   $("form#description").submit(function(event) {
     event.preventDefault();
-    const description1Input = $("input#description1");
+    const description1Input = $("input#description1").val();
+    $(".description1").text(description1Input);
+  });
+});
+
+$(document).ready(function() {
+  $("form#date").submit(function(event) {
+    event.preventDefault();
+    const date1Input = $("input#date1").val();
+    $(".date1").text(date1Input);
+  });
+});
+
+$(document).ready(function() {
+  $("form#times").submit(function(event) {
+    event.preventDefault();
+    const times1Input = $("input#times1").val();
+    $(".times1").text(times1Input);
   });
 });
